51d01737db8177de1e8dcca7604c06e1c309e487
Jude <judecodes@gmail.com>
2023-09-14 14:52:16 +0100

    feat: add disabled prop to Checkbox component
    The prop 'disabled' has been added directly to Checkbox component in this commit to control the accessibility status of the Checkbox. Previously, the 'disabled' status was only managed through a CSS class which was not as explicit or controllable.

--- src/components/global/Checkbox.jsx
+++ src/components/global/Checkbox.jsx
@@ -11,7 +11,7 @@ const Checkbox = forwardRef(({ id, children, disabled, className, ...props }, re
           "w-4 h-4 mt-1 text-primary-600 bg-gray-100 rounded-md border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600",
           { 'opacity-60 pointer-events-none': disabled }
         ) }
-        id={ id } type="checkbox" { ...props }
+        id={ id } type="checkbox" disabled={ disabled } { ...props }
       />
       {
         !!children &&
