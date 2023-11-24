5367ff707d69dff0d08975eb79062bf5be0b96a2
Jude <judecodes@gmail.com>
2023-11-24 22:08:11 +0100

    fix: update Image component for dynamic source handling
    The code update in the Image component now handles both S3 bucket images and external HTTPS sources. It does so by checking if the image source string begins with 'https://'. Additionally, a timestamp parameter has been added to Amazon S3 image paths to prevent caching issues.

--- src/components/core/shared/Image.jsx
+++ src/components/core/shared/Image.jsx
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 const bucket = import.meta.env.VITE_S3_BUCKET_NAME;
 
 const Image = ({ src, alt, ...props }) => {
-  return <img src={`https://${bucket}.s3.amazonaws.com/${src}?cache=no`} alt={alt} {...props} />;
+  return (
+    <img
+      src={src.startsWith('https://') ? src : `https://${bucket}.s3.amazonaws.com/${src}?timestamp=${Date.now()}`}
+      alt={alt}
+      {...props}
+    />
+  );
 };
 
 Image.propTypes = {
