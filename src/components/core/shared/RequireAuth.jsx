69ca5cb75a940ca09790bf4d8f0a787ee893a22c
Jude <judecodes@gmail.com>
2023-11-29 00:44:40 +0100

    feat: add search params to RequireAuth redirection
    In the RequireAuth component, the useSearchParams hook from react-router-dom is now used when checking user's authentication status. Instead of always redirecting to the current pathname on unsuccessful auth verification, it first checks for a 'from' query param. This allows the component to remember the intended page after a successful login or email verification.

--- src/components/core/shared/RequireAuth.jsx
+++ src/components/core/shared/RequireAuth.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { useAuth } from '@/hooks/use-auth.jsx';
 import Loader from '@/components/global/Loader.jsx';
 import PropTypes from 'prop-types';
@@ -7,15 +7,18 @@ import PropTypes from 'prop-types';
 const RequireAuth = ({ children }) => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [params] = useSearchParams();
+
   const { authenticated, resolved, user } = useAuth();
 
   useEffect(() => {
+    const from = params.get('from');
     if (resolved && !authenticated) {
-      navigate(`/login?from=${location.pathname}`, { replace: true });
+      navigate(`/login?from=${from || location.pathname}`, { replace: true });
     } else if (resolved && authenticated && user && !user.emailVerified) {
-      navigate(`/verification?from=${location.pathname}`);
+      navigate(`/verification?from=${from || location.pathname}`);
     }
-  }, [resolved, authenticated, user, navigate, location.pathname]);
+  }, [resolved, authenticated, user, navigate, location.pathname, params]);
 
   if (resolved && authenticated && user?.emailVerified) return children;
 
