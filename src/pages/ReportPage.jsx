178cd78ebd11d4363ce18337f6a57464474e469a
Jude <judecodes@gmail.com>
2024-06-22 13:26:54 +0100

    change container max width to 1000px

--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import ProjectHeader from '../components/core/report/components/ProjectHeader';
 import ReportBody from '../components/core/report/components/ReportBody';
 
 const ReportPage = () => {
   return (
-    <div className="container py-10">
-        <ProjectHeader />
-        <ReportBody />
+    <div className="container !max-w-[1100px] py-10">
+      <ProjectHeader />
+      <ReportBody />
     </div>
-  )
-}
+  );
+};
 
 export default ReportPage;
