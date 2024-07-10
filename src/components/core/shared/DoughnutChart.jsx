dda0c66d7e4e7e13ad577ffdae21b3702555559a
Oyegbile Praise <83648252+oyegbilepraise@users.noreply.github.com>
2024-07-10 03:06:33 +0100

    add more chart components

--- src/components/core/shared/DoughnutChart.jsx
+++ src/components/core/shared/DoughnutChart.jsx
@@ -1,80 +1,66 @@
-import { Card } from '@nextui-org/react';
 import classNames from 'classnames';
 import { useState } from 'react';
-import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer, Label } from 'recharts';
+import { Cell, Label, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
+import PropTypes from 'prop-types';
 
 const COLORS = ['#2D4739', '#A0C1B9', '#706993'];
 
-const DoughnutChart = ({ data }) => {
-  const [activeIndex, setActiveIndex] = useState(data[0]);
 const DoughnutChart = ({ data }) => {
   const [activeIndex, setActiveIndex] = useState(data[0]);
 
   const renderLegend = (args) => {
     const { payload } = args;
     return (
-      <div className="flex flex-col gap-2 -translate-x-5">
+      <div className="flex flex-col gap-2 -translate-x-10">
         {payload.map((entry, index) => (
-          <div className="flex border dark:border-[#333] rounded p-2 gap-2 items-center" key={`item-${index}`}>
+          <div
+            className="flex border dark:border-default-200 px-4 py-1 gap-2 rounded-full items-center"
+            key={`item-${index}`}
+          >
             <div
-              className={classNames(
-                'w-2 h-2 rounded-full',
-                entry.value === 'United States' && 'bg-[#03bcc2]',
-                entry.value === 'United Kingdom' && 'bg-[#2296f3]',
-                entry.value === 'Germany' && 'bg-[#df5858]'
-              )}
-            ></div>
-
-            <span className="text-gray-500 dark:text-gray-400"> {entry.value}</span>
+              className={classNames('w-2 h-2 rounded-full')}
+              style={{ backgroundColor: COLORS[index % COLORS.length] }}
+            />
+            <span className="text-gray-500 dark:text-gray-400">{entry.value}</span>
           </div>
         ))}
       </div>
     );
   };
-  };
 
   const onPieClick = (_, index) => {
-    setActiveIndex(data[index]);
     setActiveIndex(data[index]);
   };
 
   return (
-    <Card className='w-full bg-white space-y-6 px-8 py-6 mt-10'>
-      <ResponsiveContainer width="100%" height={240}>
-        <PieChart>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            innerRadius={60}
-            outerRadius={80}
-            fill="#8884d8"
-            paddingAngle={2}
-            dataKey="value"
-            onClick={onPieClick}
-          >
-            <Label
-              value={`${activeIndex?.value}%`}
-              position="centerBottom"
-              className=" -translate-y-2 !mb-2 font-semibold"
-              fontSize="27px "
-            />
-
-            <Label
-              value={activeIndex?.name?.toLocaleUpperCase()}
-              position="centerTop"
-              className="text-xs mt-1 translate-y-2 "
-            />
-
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-          <Tooltip />
-          <Legend layout="vertical" verticalAlign="middle" align="right" iconType="circle" content={renderLegend} />
-        </PieChart>
-      </ResponsiveContainer>
-    </Card>
+    <ResponsiveContainer width="100%" height={200}>
+      <PieChart>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          innerRadius={50}
+          outerRadius={68}
+          paddingAngle={0}
+          dataKey="value"
+          onClick={onPieClick}
+          stroke="0"
+        >
+          <Label
+            value={`${activeIndex?.value}%`}
+            position="center"
+            className="font-semibold -translate-y-3"
+            fontSize="20px"
+          />
+          <Label value={activeIndex?.name} position="center" className="translate-y-3" fontSize="11px" />
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend layout="vertical" verticalAlign="middle" align="right" iconType="circle" content={renderLegend} />
+      </PieChart>
+    </ResponsiveContainer>
   );
 };
 
@@ -88,4 +74,3 @@ DoughnutChart.propTypes = {
 };
 
 export default DoughnutChart;
-
