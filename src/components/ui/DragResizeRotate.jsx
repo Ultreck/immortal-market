9580fb7f88db1fa26eb9065a719a2a766a0a886b
Jude <judecodes@gmail.com>
2024-08-19 11:37:49 +0100

    fix: warnings from @use-gesture

--- src/components/ui/DragResizeRotate.jsx
+++ src/components/ui/DragResizeRotate.jsx
@@ -168,7 +168,7 @@ const DraggableResizableRotatable = ({
         {draggable && (
           <div
             data-name="drag"
-            className="absolute inset-0 w-full h-full"
+            className="absolute inset-0 w-full h-full touch-none"
             {...bindDrag()}
             onDoubleClick={onDragDblClick}
           />
@@ -178,56 +178,56 @@ const DraggableResizableRotatable = ({
             {!!handles.includes('se') && (
               <div
                 data-name="resize-se"
-                className="cursor-se-resize absolute -bottom-2 -right-2 w-4 h-4 border border-gray-500 rounded-full"
+                className="cursor-se-resize absolute -bottom-2 -right-2 w-4 h-4 border border-gray-500 rounded-full touch-none"
                 {...bingResize()}
               />
             )}
             {!!handles.includes('ne') && (
               <div
                 data-name="resize-ne"
-                className="cursor-ne-resize absolute -top-2 -right-2 w-4 h-4 border border-gray-500 rounded-full"
+                className="cursor-ne-resize absolute -top-2 -right-2 w-4 h-4 border border-gray-500 rounded-full touch-none"
                 {...bingResize()}
               />
             )}
             {!!handles.includes('nw') && (
               <div
                 data-name="resize-nw"
-                className="cursor-nw-resize absolute -top-2 -left-2 w-4 h-4 border border-gray-500 rounded-full"
+                className="cursor-nw-resize absolute -top-2 -left-2 w-4 h-4 border border-gray-500 rounded-full touch-none"
                 {...bingResize()}
               />
             )}
             {!!handles.includes('sw') && (
               <div
                 data-name="resize-sw"
-                className="cursor-sw-resize absolute -bottom-2 -left-2 w-4 h-4 border border-gray-500 rounded-full"
+                className="cursor-sw-resize absolute -bottom-2 -left-2 w-4 h-4 border border-gray-500 rounded-full touch-none"
                 {...bingResize()}
               />
             )}
             {!!handles.includes('e') && (
               <div
                 data-name="resize-e"
-                className="cursor-e-resize absolute top-1/2 -translate-y-1/2 -right-1 w-2 h-8 max-h-[98%] border border-gray-500 rounded-full"
+                className="cursor-e-resize absolute top-1/2 -translate-y-1/2 -right-1 w-2 h-8 max-h-[98%] border border-gray-500 rounded-full touch-none"
                 {...bingResize()}
               />
             )}
             {!!handles.includes('w') && (
               <div
                 data-name="resize-w"
-                className="cursor-e-resize absolute top-1/2 -translate-y-1/2 -left-1 w-2 h-8 max-h-[98%] border border-gray-500 rounded-full"
+                className="cursor-e-resize absolute top-1/2 -translate-y-1/2 -left-1 w-2 h-8 max-h-[98%] border border-gray-500 rounded-full touch-none"
                 {...bingResize()}
               />
             )}
             {!!handles.includes('n') && (
               <div
                 data-name="resize-n"
-                className="cursor-n-resize absolute -top-1 -translate-x-1/2 left-1/2 w-8 h-2 max-w-[98%] border border-gray-500 rounded-full"
+                className="cursor-n-resize absolute -top-1 -translate-x-1/2 left-1/2 w-8 h-2 max-w-[98%] border border-gray-500 rounded-full touch-none"
                 {...bingResize()}
               />
             )}
             {!!handles.includes('s') && (
               <div
                 data-name="resize-s"
-                className="cursor-s-resize absolute -bottom-1 -translate-x-1/2 left-1/2 w-8 h-2 max-w-[98%] border border-gray-500 rounded-full"
+                className="cursor-s-resize absolute -bottom-1 -translate-x-1/2 left-1/2 w-8 h-2 max-w-[98%] border border-gray-500 rounded-full touch-none"
                 {...bingResize()}
               />
             )}
