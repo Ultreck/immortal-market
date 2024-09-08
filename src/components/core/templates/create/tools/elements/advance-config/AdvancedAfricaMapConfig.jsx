d4e5c4a476ff2bb6e9606b7ca0f2fec782689009
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2024-09-08 18:43:42 +0100

    Made the required changes to all existing map, such changes are: being able to manually add name of place and value on the map and all other conditional implementation tha required

--- src/components/core/templates/create/tools/elements/advance-config/AdvancedAfricaMapConfig.jsx
+++ src/components/core/templates/create/tools/elements/advance-config/AdvancedAfricaMapConfig.jsx
@@ -83,6 +83,7 @@ const AdvancedAfricaMapConfig = ({ element, onChange }) => {
       <Tab key="create" title="Create" className="text-base">
         <form onSubmit={handleSubmit(handleAddMapValues)}>
           <div className="">
+          <h1 className="text-lg font-semibold">Enter both value and name of state/country</h1>
             <div className="text my-5">
               <input
                 type="text"
