c4b08f74f0264364ef3a62a61707eac93ec219f1
Chimdia Anyiam <fortunechimdiya@gmail.com>
2024-09-23 23:22:55 +0100

    feat(templates): Added cell edit tool

--- src/components/core/templates/create/elements/Table.jsx
+++ src/components/core/templates/create/elements/Table.jsx
@@ -2,10 +2,25 @@ import ElementWrapper from '@/components/core/templates/create/ElementWrapper.js
 import PropTypes from 'prop-types';
 import { ElementPropTypes } from '@/lib/prop-types.js';
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@nextui-org/react';
-import { RiAddLine, RiArrowDownSLine } from 'react-icons/ri';
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Select,
+  SelectItem,
+} from '@nextui-org/react';
+import { RiAddLine, RiArrowDownSLine, RiFontFamily } from 'react-icons/ri';
 import { cn } from '@/lib/utils.js';
 import AutoResizeTextArea from '@/components/ui/AutoResizeTextArea.jsx';
+import useResolveValue from '@/hooks/template/use-resolve-value';
+import { TbBold, TbItalic, TbUnderline } from 'react-icons/tb';
+import ColorPicker from '@/components/ui/ColorPicker';
+import AutoCompleteNumberInput from '@/components/ui/AutoCompleteNumberInput';
 
 const Table = ({ element, active, highlighted, width, onClick, onChange }) => {
   return (
@@ -227,6 +242,69 @@ const TableContent = ({ element, onChange, active }) => {
     setSelection(null);
   }, [selection, rows, onChange, element]);
 
+  const handleUpdateCellsStyle = useCallback(
+    (newStyle) => {
+      if (!selection) return;
+
+      const { startRow, startCol, endRow, endCol } = selection;
+      const [minRow, maxRow] = [Math.min(startRow, endRow), Math.max(startRow, endRow)];
+      const [minCol, maxCol] = [Math.min(startCol, endCol), Math.max(startCol, endCol)];
+      let newRows = JSON.parse(JSON.stringify(rows));
+
+      for (let rowIndex = minRow; rowIndex <= maxRow; rowIndex++) {
+        for (let cellIndex = minCol; cellIndex <= maxCol; cellIndex++) {
+          const cell = newRows[rowIndex].cells[cellIndex];
+          const colSpan = cell.colSpan || 1;
+          const rowSpan = cell.rowSpan || 1;
+          newRows[rowIndex].cells[cellIndex] = {
+            ...cell,
+            style: { ...cell.style, ...newStyle },
+          };
+          if (colSpan > 1) {
+            for (let k = 0; k < colSpan - 1; k++) {
+              newRows[rowIndex].cells = newRows[rowIndex].cells.toSpliced(cellIndex + k, 0, {
+                ...newRows[rowIndex].cells[cellIndex + k],
+                style: { ...newRows[rowIndex].cells[cellIndex + k]?.style, ...newStyle },
+              });
+            }
+          }
+          if (rowSpan > 1) {
+            for (let k = 0; k < rowSpan - 1; k++) {
+              for (let l = 0; l < colSpan; l++) {
+                newRows[rowIndex + k + 1].cells = newRows[rowIndex + k + 1].cells.toSpliced(cellIndex + l, 0, {
+                  ...newRows[rowIndex + k + 1].cells[cellIndex + l],
+                  style: { ...newRows[rowIndex + k + 1].cells[cellIndex + l]?.style, ...newStyle },
+                });
+              }
+            }
+          }
+        }
+      }
+
+      onChange({ ...element, config: { ...element.config, data: newRows } });
+    },
+    [selection, rows, onChange, element]
+  );
+
+  const getSelectedCells = (rows, selection) => {
+    if (!selection) return [];
+    const { startRow, startCol, endRow, endCol } = selection;
+
+    const selectedCells = [];
+    const [minRow, maxRow] = [Math.min(startRow, endRow), Math.max(startRow, endRow)];
+    const [minCol, maxCol] = [Math.min(startCol, endCol), Math.max(startCol, endCol)];
+
+    for (let rowIndex = minRow; rowIndex <= maxRow; rowIndex++) {
+      for (let colIndex = minCol; colIndex <= maxCol; colIndex++) {
+        selectedCells.push(rows[rowIndex].cells[colIndex]);
+      }
+    }
+
+    return selectedCells;
+  };
+
+  const selectedCells = getSelectedCells(rows, selection);
+
   return (
     <>
       {!!selection && (
@@ -281,6 +359,25 @@ const TableContent = ({ element, onChange, active }) => {
               </DropdownItem>
             </DropdownMenu>
           </Dropdown>
+          <Popover placement="bottom" showArrow offset={10}>
+            <PopoverTrigger>
+              <Button isIconOnly variant="flat" aria-label="Edit Cell" className="text-base h-8">
+                <RiFontFamily size="16" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="p-0 shadow border border-default-200">
+              <div className="px-6 py-6 w-full space-y-4">
+                <div className="flex items-center space-x-2">
+                  <Italic selectedCells={selectedCells} handleUpdateCellsStyle={handleUpdateCellsStyle} />
+                  <Bold selectedCells={selectedCells} handleUpdateCellsStyle={handleUpdateCellsStyle} />
+                  <Underline selectedCells={selectedCells} handleUpdateCellsStyle={handleUpdateCellsStyle} />
+                  <TextColor selectedCells={selectedCells} handleUpdateCellsStyle={handleUpdateCellsStyle} />
+                </div>
+                <FontFamily selectedCells={selectedCells} handleUpdateCellsStyle={handleUpdateCellsStyle} />
+                <FontSize selectedCells={selectedCells} handleUpdateCellsStyle={handleUpdateCellsStyle} />
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
       )}
       <table
@@ -468,3 +565,194 @@ TablePreview.propTypes = {
 };
 
 export default Table;
+
+const Italic = ({ selectedCells, handleUpdateCellsStyle }) => {
+  const value = useResolveValue(selectedCells.map((cell) => cell.style.fontStyle));
+
+  return (
+    <Button
+      isIconOnly
+      variant={value === 'italic' ? 'solid' : 'text'}
+      aria-label="Italicize/unitalicize text"
+      onClick={() => {
+        const newStyle = {
+          fontStyle: value === 'italic' ? 'normal' : 'italic',
+        };
+
+        handleUpdateCellsStyle(newStyle);
+      }}
+    >
+      <TbItalic size="20" />
+    </Button>
+  );
+};
+const Underline = ({ selectedCells, handleUpdateCellsStyle }) => {
+  const value = useResolveValue(selectedCells.map((cell) => cell.style.textDecoration));
+
+  return (
+    <Button
+      isIconOnly
+      variant={value === 'underline' ? 'solid' : 'text'}
+      aria-label="Underline/unbold text"
+      onClick={() => {
+        const newStyle = {
+          textDecoration: value === 'underline' ? 'none' : 'underline',
+        };
+
+        handleUpdateCellsStyle(newStyle);
+      }}
+    >
+      <TbUnderline size="20" />
+    </Button>
+  );
+};
+
+const Bold = ({ selectedCells, handleUpdateCellsStyle }) => {
+  const value = useResolveValue(selectedCells.map((cell) => cell.style.fontWeight));
+
+  return (
+    <Button
+      isIconOnly
+      variant={value === 'bold' ? 'solid' : 'text'}
+      aria-label="Bold/unbold text"
+      onClick={() => {
+        const newStyle = {
+          fontWeight: value === 'bold' ? 'normal' : 'bold',
+        };
+
+        handleUpdateCellsStyle(newStyle);
+      }}
+    >
+      <TbBold size="20" />
+    </Button>
+  );
+};
+
+const fonts = [
+  { key: 'Roboto', label: 'Roboto' },
+  { key: 'Playwrite BE VLG', label: 'Playwrite BE VLG' },
+  { key: 'Kanit', label: 'Kanit' },
+  { key: 'Lato', label: 'Lato' },
+  { key: 'Open Sans', label: 'Open Sans' },
+  { key: 'Poppins', label: 'Poppins' },
+  { key: 'Montserrat', label: 'Montserrat' },
+  { key: 'Oswald', label: 'Oswald' },
+  { key: 'Raleway', label: 'Raleway' },
+  { key: 'Inter', label: 'Inter' },
+  { key: 'Noto Sans', label: 'Noto Sans' },
+  { key: 'Playfair Display', label: 'Playfair Display' },
+  { key: 'Rubik', label: 'Rubik' },
+  { key: 'Nunito', label: 'Nunito' },
+  { key: 'PT Sans', label: 'PT Sans' },
+  { key: 'Work Sans', label: 'Work Sans' },
+  { key: 'Libre Baskerville', label: 'Libre Baskerville' },
+  { key: 'Manrope', label: 'Manrope' },
+  { key: 'Source Sans 3', label: 'Source Sans 3' },
+  { key: 'Hahmlet', label: 'Hahmlet' },
+];
+const FontFamily = ({ selectedCells, handleUpdateCellsStyle }) => {
+  const useResolveValue = (values, fallback = '') => {
+    const same = values.every((v) => v === values[0]);
+    return same ? values[0] : fallback;
+  };
+
+  const value = useResolveValue(selectedCells.map((cell) => cell.style.fontFamily));
+  const handleChange = (event) => {
+    const newStyle = {
+      fontFamily: event.target.value,
+    };
+    handleUpdateCellsStyle(newStyle);
+  };
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <p className="text-base opacity-75 whitespace-nowrap">Font family:</p>
+      <Select
+        aria-label="Select font family"
+        placeholder="Select font family"
+        classNames={{ value: 'text-base px-2', popoverContent: 'bg-default-100' }}
+        onChange={handleChange}
+        defaultSelectedKeys={[value]}
+      >
+        {fonts.map((font) => (
+          <SelectItem key={font.key}>{font.label}</SelectItem>
+        ))}
+      </Select>
+    </div>
+  );
+};
+
+const TextColor = ({ selectedCells, handleUpdateCellsStyle }) => {
+ 
+  const value = useResolveValue(selectedCells.map((cell) => cell.style.color));
+
+  const handleChange = (color) => {
+    if (!color) return;
+    const newStyle = { color };
+    handleUpdateCellsStyle(newStyle);
+  };
+
+  return (
+    <div>
+      <ColorPicker
+        color={value}
+        onChange={(color) => handleChange(color)}
+        trigger={
+          <Button variant="text" isIconOnly className="text-base">
+            <div className="w-6 flex flex-col items-center justify-center">
+              <RiFontFamily size="16" />
+              <div className="rounded-2xl h-1.5 mt-0.5 w-full" style={{ background: value }}></div>
+            </div>
+          </Button>
+        }
+      />
+    </div>
+  );
+};
+
+const FontSize = ({ selectedCells, handleUpdateCellsStyle }) => {
+  // Helper hook to check if all selected cells have the same font size
+  const useResolveValue = (values, fallback = '') => {
+    const same = values.every((v) => v === values[0]);
+    return same ? values[0] : fallback;
+  };
+
+  // Determine the font size for the selected cells
+  const value = useResolveValue(selectedCells.map((cell) => cell.style.fontSize));
+
+  // Handle font size change
+  const handleChange = (newValue) => {
+    if (newValue === '') return;
+    // Apply the new font size to the selected cells
+    const newStyle = { fontSize: +newValue }; // Convert to number
+    handleUpdateCellsStyle(newStyle);
+  };
+
+  return (
+    <div className="flex items-center justify-between space-x-4">
+      <p className="text-base opacity-75">Font size:</p>
+      <AutoCompleteNumberInput
+        onChange={handleChange}
+        value={value}
+        min={1}
+        max={150}
+        step={1}
+        ariaLabel="Font size"
+      />
+    </div>
+  );
+};
+
+
+const propTypes = {
+  handleUpdateCellsStyle: PropTypes.func.isRequired,
+  selectedCells: PropTypes.array.isRequired,
+};
+
+Italic.propTypes = propTypes;
+Underline.propTypes = propTypes;
+Bold.propTypes = propTypes;
+FontFamily.propTypes = propTypes;
+TextColor.propTypes = propTypes;
+FontSize.propTypes = propTypes;
+
--- src/components/core/templates/create/sidebar/data/Tables.jsx
+++ src/components/core/templates/create/sidebar/data/Tables.jsx
@@ -17,31 +17,40 @@ const colors = [
   '#D93566',
 ];
 
+const defaultCellStyles = {
+  fontSize: 14,
+  fontWeight: 'normal',
+  color: '#000000',
+  textAlign: 'left',
+  opacity: 1,
+  fontFamily: 'Roboto',
+};
+
 const elements = [
   {
     data: [
       {
         cells: [
-          { value: 'Key 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Value 1', colSpan: 1, rowSpan: 1 },
+          { value: 'Key 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Value 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Key 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Value 2', colSpan: 1, rowSpan: 1 },
+          { value: 'Key 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Value 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Key 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Value 3', colSpan: 1, rowSpan: 1 },
+          { value: 'Key 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Value 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Key 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Value 4', colSpan: 1, rowSpan: 1 },
+          { value: 'Key 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Value 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
     ],
@@ -52,34 +61,39 @@ const elements = [
     data: [
       {
         cells: [
-          { value: 'Heading 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 4', colSpan: 1, rowSpan: 1 },
+          {
+            value: 'Heading 1',
+            colSpan: 1,
+            rowSpan: 1,
+            style: defaultCellStyles,
+          },
+          { value: 'Heading 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Cell 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 4', colSpan: 1, rowSpan: 1 },
+          { value: 'Cell 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Cell 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 5', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 6', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 7', colSpan: 1, rowSpan: 1 },
+          { value: 'Cell 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 6', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 7', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Cell 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 5', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 6', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 7', colSpan: 1, rowSpan: 1 },
+          { value: 'Cell 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 6', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 7', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
     ],
@@ -90,34 +104,34 @@ const elements = [
     data: [
       {
         cells: [
-          { value: 'S/N', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 3', colSpan: 1, rowSpan: 1 },
+          { value: 'S/N', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: '1', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 4', colSpan: 1, rowSpan: 1 },
+          { value: '1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: '2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 5', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 6', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 7', colSpan: 1, rowSpan: 1 },
+          { value: '2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 6', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 7', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: '3', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 5', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 6', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 7', colSpan: 1, rowSpan: 1 },
+          { value: '3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 6', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 7', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
     ],
@@ -128,50 +142,50 @@ const elements = [
     data: [
       {
         cells: [
-          { value: '', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 5', colSpan: 1, rowSpan: 1 },
+          { value: '', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: '1', colSpan: 1, rowSpan: 2 },
-          { value: 'Cell 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 5', colSpan: 1, rowSpan: 1 },
+          { value: '1', colSpan: 1, rowSpan: 2, style: defaultCellStyles },
+          { value: 'Cell 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Cell 8', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 9', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 10', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 11', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 12', colSpan: 1, rowSpan: 1 },
+          { value: 'Cell 8', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 9', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 10', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 11', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 12', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: '1', colSpan: 1, rowSpan: 2 },
-          { value: 'Cell 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 5', colSpan: 1, rowSpan: 1 },
+          { value: '1', colSpan: 1, rowSpan: 2, style: defaultCellStyles },
+          { value: 'Cell 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Cell 8', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 9', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 10', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 11', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 12', colSpan: 1, rowSpan: 1 },
+          { value: 'Cell 8', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 9', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 10', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 11', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 12', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
     ],
@@ -182,54 +196,54 @@ const elements = [
     data: [
       {
         cells: [
-          { value: '', colSpan: 1, rowSpan: 1 },
-          { value: 'Title 1', colSpan: 2, rowSpan: 1 },
-          { value: 'Title 2', colSpan: 2, rowSpan: 1 },
-          { value: 'Title 3', colSpan: 2, rowSpan: 1 },
+          { value: '', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Title 1', colSpan: 2, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Title 2', colSpan: 2, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Title 3', colSpan: 2, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
           { value: '', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 5', colSpan: 1, rowSpan: 1 },
-          { value: 'Heading 6', colSpan: 1, rowSpan: 1 },
+          { value: 'Heading 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Heading 6', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Label 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 5', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 6', colSpan: 1, rowSpan: 1 },
+          { value: 'Label 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 6', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Label 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 5', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 6', colSpan: 1, rowSpan: 1 },
+          { value: 'Label 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 6', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
       {
         cells: [
-          { value: 'Label 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 1', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 2', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 3', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 4', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 5', colSpan: 1, rowSpan: 1 },
-          { value: 'Cell 6', colSpan: 1, rowSpan: 1 },
+          { value: 'Label 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 1', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 2', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 3', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 4', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 5', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
+          { value: 'Cell 6', colSpan: 1, rowSpan: 1, style: defaultCellStyles },
         ],
       },
     ],
@@ -299,3 +313,4 @@ Tables.propTypes = {
 };
 
 export default Tables;
+
