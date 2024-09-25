eaacdab3e230c448df1e9c07de2f86c8f2c0b25f
Oyegbile Praise <83648252+oyegbilepraise@users.noreply.github.com>
2024-09-25 06:11:14 +0100

    feat(chart): Menu restructuring

--- src/components/core/templates/create/sidebar/charts/Charts.jsx
+++ src/components/core/templates/create/sidebar/charts/Charts.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 import StandardCharts from '../data/StandardCharts';
 import AdvancedCharts from '../data/AdvanceCharts';
 import Maps from '../data/Maps';
+import { TbChartBar, TbChartPie, TbMap, TbWorld } from 'react-icons/tb';
 
 const Charts = () => {
   return (
     <Accordion variant="splitted">
-      <AccordionItem key="1" aria-label="Standard Charts" title="Standard Charts">
+      <AccordionItem key="1" aria-label="Standard Charts" title="Standard Charts" startContent={<TbChartPie size="25" />}>
         <StandardCharts />
       </AccordionItem>
-      <AccordionItem key="2" aria-label="Advanced Charts" title="Advanced Charts">
+      <AccordionItem key="2" aria-label="Advanced Charts" title="Advanced Charts" startContent={<TbChartBar size="25" />}>
         <AdvancedCharts />
       </AccordionItem>
-      <AccordionItem key="3" aria-label="Maps" title="Maps">
+      <AccordionItem key="3" aria-label="Maps" title="Maps" startContent={<TbWorld size="25" />}>
         <Maps />
       </AccordionItem>
     </Accordion>
