d3dfed901134ee59bb29ebe022a29b224fc316a6
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2024-09-30 05:42:33 +0100

    Created dashboard and called it home

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ import AiAssistantPage from '@/pages/AiAssistantPage.jsx';
 import InvitationPage from '@/pages/InvitationPage.jsx';
 import PresentDesignPage from '@/pages/designs/PresentDesignPage.jsx';
 import SvgShapes from '@/components/core/SvgShapes.jsx';
+import LandingPage from './pages/home/LandingPage.jsx';
 
 const App = () => {
   const { isDarkMode } = useTernaryDarkMode();
@@ -66,8 +67,9 @@ const App = () => {
           }
         >
           <Route path="" element={<OverviewPage />} />
-          <Route path="overview" element={<Navigate to="/" replace />} />
-          <Route path="projects" element={<ProjectsPage />} />
+          <Route path="/overview" element={<Navigate to="/" replace />} />
+          <Route path="projects" element={<ProjectPage />} />
+          <Route path="home" element={<LandingPage />} />
           <Route path="templates" element={<TemplatesPage />} />
           <Route path="dashboards" element={<ProjectDashboardPage />} />
           <Route path="outsource" element={<OutsourcePage />} />
