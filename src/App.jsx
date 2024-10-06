2e9f785a923e08077ba5202ed300a8d1a6d28949
Jude <judecodes@gmail.com>
2024-10-06 05:06:29 +0100

    chore: remove LandingPage components

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,6 @@ import AiAssistantPage from '@/pages/AiAssistantPage.jsx';
 import InvitationPage from '@/pages/InvitationPage.jsx';
 import PresentDesignPage from '@/pages/designs/PresentDesignPage.jsx';
 import SvgShapes from '@/components/core/SvgShapes.jsx';
-import LandingPage from './pages/home/LandingPage.jsx';
 import ProjectsPage from './pages/ProjectsPage.jsx';
 
 const App = () => {
@@ -69,7 +68,6 @@ const App = () => {
           <Route path="" element={<OverviewPage />} />
           <Route path="/overview" element={<Navigate to="/" replace />} />
           <Route path="projects" element={<ProjectsPage />} />
-          <Route path="home" element={<LandingPage />} />
           <Route path="templates" element={<TemplatesPage />} />
           <Route path="dashboards" element={<ProjectDashboardPage />} />
           <Route path="outsource" element={<OutsourcePage />} />
