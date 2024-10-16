e9fc5cb69f4b3026c0572b1d0417c3ebd9a8322f
Jude <judecodes@gmail.com>
2024-10-16 01:50:46 +0100

    feat: update overview page

--- src/components/core/overview/PopularTemplates.jsx
+++ src/components/core/overview/PopularTemplates.jsx
@@ -1,4 +1,4 @@
-import { Skeleton } from '@nextui-org/react';
+import { Chip, Skeleton } from '@nextui-org/react';
 import DesignCard from '@/components/core/project/DesignCard.jsx';
 import NoData from '@/components/ui/NoData.jsx';
 import { useGetTemplates } from '@/api/business.js';
@@ -8,7 +8,24 @@ const PopularTemplates = () => {
 
   return (
     <section>
-      <h2 className="font-semibold text-xl mb-6">Start with a template</h2>
+      <h2 className="font-semibold text-xl mb-4">Start with a template</h2>
+      <div className="mb-6 flex flex-wrap gap-3">
+        <Chip variant="solid" size="md" classNames={{ base: 'cursor-pointer', content: 'font-medium' }} color="primary">
+          All
+        </Chip>
+        <Chip variant="flat" size="md" classNames={{ base: 'cursor-pointer', content: 'font-medium' }}>
+          Tech
+        </Chip>
+        <Chip variant="flat" size="md" classNames={{ base: 'cursor-pointer', content: 'font-medium' }}>
+          Customer
+        </Chip>
+        <Chip variant="flat" size="md" classNames={{ base: 'cursor-pointer', content: 'font-medium' }}>
+          Marketing
+        </Chip>
+        <Chip variant="flat" size="md" classNames={{ base: 'cursor-pointer', content: 'font-medium' }}>
+          Report
+        </Chip>
+      </div>
       {isDesignsLoading ? (
         <div className="grid grid-cols-4 gap-4 md:gap-8">
           <Skeleton className="aspect-square w-full rounded-2xl" />
