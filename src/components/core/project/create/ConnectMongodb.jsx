6759ee3725074b6e29cd068e5b9b87b72d5ba808
Jude <judecodes@gmail.com>
2024-10-21 04:43:30 +0100

    chore(create-project): code cleanup

--- src/components/core/project/create/ConnectMongodb.jsx
+++ src/components/core/project/create/ConnectMongodb.jsx
@@ -15,20 +15,16 @@ const ConnectMongodb = ({ onPrev, onNext }) => {
   return (
     <>
       {view === 'form' && <Form onPrev={onPrev} onNext={() => setView('tables')} />}
-      {view === 'tables' && <Tables onPrev={() => setView('form')} onNext={onNext} />}
+      {view === 'tables' && <Collections onPrev={() => setView('form')} onNext={onNext} />}
     </>
   );
 };
 
 const Form = ({ onPrev, onNext }) => {
-  const [type] = useState('mongodb');
-
   const toast = useToast();
   const { id: business } = useBusiness();
   const credentials = useCreateProjectStore((state) => state.data.credentials);
   const updateData = useCreateProjectStore((state) => state.updateData);
-  const { mutateAsync: connect, isPending: isConnecting } = useGetDatabaseTables(business);
-
   const {
     handleSubmit,
     formState: { isValid },
@@ -39,9 +35,11 @@ const Form = ({ onPrev, onNext }) => {
       ...credentials,
     },
   });
+  const { mutateAsync: connect, isPending: isConnecting } = useGetDatabaseTables(business);
 
   const onSubmit = async (values) => {
     if (!isValid) return;
+    const type = 'mongodb';
     try {
       const res = await connect({ payload: values, type });
       updateData({ credentials: { ...values, type }, tables: res.data.collections });
@@ -54,124 +52,126 @@ const Form = ({ onPrev, onNext }) => {
   return (
     <div className="flex flex-col">
       <Title title="Connect to Mongodb" sub="Import data from your Mongodb database" className="mb-10" />
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-        <div className="grid grid-cols-2 gap-4">
-          <div className="grid grid-cols-1 gap-2">
-            <p>Host</p>
-            <Controller
-              name="host"
-              control={control}
-              rules={{ required: 'Host is required' }}
-              disabled={isConnecting}
-              render={({ field, fieldState: { error } }) => (
-                <Input
-                  placeholder="Enter host"
-                  variant="bordered"
-                  size="lg"
-                  value={field.value}
-                  onChange={field.onChange}
-                  classNames={{ input: 'text-base px-2' }}
-                  errorMessage={error?.message}
-                  isInvalid={!!error?.message}
-                  isDisabled={field.disabled}
-                />
-              )}
-            />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-6">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 gap-2">
+              <p>Host</p>
+              <Controller
+                name="host"
+                control={control}
+                rules={{ required: 'Host is required' }}
+                disabled={isConnecting}
+                render={({ field, fieldState: { error } }) => (
+                  <Input
+                    placeholder="Enter host"
+                    variant="bordered"
+                    size="lg"
+                    value={field.value}
+                    onChange={field.onChange}
+                    classNames={{ input: 'text-base px-2' }}
+                    errorMessage={error?.message}
+                    isInvalid={!!error?.message}
+                    isDisabled={field.disabled}
+                  />
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-1 gap-2">
+              <p>Database name</p>
+              <Controller
+                name="database"
+                control={control}
+                rules={{ required: 'Database name is required' }}
+                disabled={isConnecting}
+                render={({ field, fieldState: { error } }) => (
+                  <Input
+                    placeholder="Enter database name"
+                    variant="bordered"
+                    size="lg"
+                    value={field.value}
+                    onChange={field.onChange}
+                    classNames={{ input: 'text-base px-2' }}
+                    errorMessage={error?.message}
+                    isInvalid={!!error?.message}
+                    isDisabled={field.disabled}
+                  />
+                )}
+              />
+            </div>
           </div>
-          <div className="grid grid-cols-1 gap-2">
-            <p>Database name</p>
-            <Controller
-              name="database"
-              control={control}
-              rules={{ required: 'Database name is required' }}
-              disabled={isConnecting}
-              render={({ field, fieldState: { error } }) => (
-                <Input
-                  placeholder="Enter database name"
-                  variant="bordered"
-                  size="lg"
-                  value={field.value}
-                  onChange={field.onChange}
-                  classNames={{ input: 'text-base px-2' }}
-                  errorMessage={error?.message}
-                  isInvalid={!!error?.message}
-                  isDisabled={field.disabled}
-                />
-              )}
-            />
+          <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 gap-2">
+              <p>Username</p>
+              <Controller
+                name="user"
+                control={control}
+                rules={{ required: 'Username is required' }}
+                disabled={isConnecting}
+                render={({ field, fieldState: { error } }) => (
+                  <Input
+                    placeholder="Enter username"
+                    variant="bordered"
+                    size="lg"
+                    value={field.value}
+                    onChange={field.onChange}
+                    classNames={{ input: 'text-base px-2' }}
+                    errorMessage={error?.message}
+                    isInvalid={!!error?.message}
+                    isDisabled={field.disabled}
+                    autoComplete="off"
+                  />
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-1 gap-2">
+              <p>Password</p>
+              <Controller
+                name="password"
+                control={control}
+                rules={{ required: 'Password is required' }}
+                disabled={isConnecting}
+                render={({ field, fieldState: { error } }) => (
+                  <Input
+                    placeholder="Enter password"
+                    type="password"
+                    variant="bordered"
+                    size="lg"
+                    value={field.value}
+                    onChange={field.onChange}
+                    classNames={{ input: 'text-base px-2' }}
+                    errorMessage={error?.message}
+                    isInvalid={!!error?.message}
+                    isDisabled={field.disabled}
+                    autoComplete="new-password"
+                  />
+                )}
+              />
+            </div>
           </div>
-        </div>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="grid grid-cols-1 gap-2">
-            <p>Username</p>
-            <Controller
-              name="user"
-              control={control}
-              rules={{ required: 'Username is required' }}
-              disabled={isConnecting}
-              render={({ field, fieldState: { error } }) => (
-                <Input
-                  placeholder="Enter username"
-                  variant="bordered"
-                  size="lg"
-                  value={field.value}
-                  onChange={field.onChange}
-                  classNames={{ input: 'text-base px-2' }}
-                  errorMessage={error?.message}
-                  isInvalid={!!error?.message}
-                  isDisabled={field.disabled}
-                  autoComplete="off"
-                />
-              )}
-            />
-          </div>
-          <div className="grid grid-cols-1 gap-2">
-            <p>Password</p>
-            <Controller
-              name="password"
-              control={control}
-              rules={{ required: 'Password is required' }}
-              disabled={isConnecting}
-              render={({ field, fieldState: { error } }) => (
-                <Input
-                  placeholder="Enter password"
-                  type="password"
-                  variant="bordered"
-                  size="lg"
-                  value={field.value}
-                  onChange={field.onChange}
-                  classNames={{ input: 'text-base px-2' }}
-                  errorMessage={error?.message}
-                  isInvalid={!!error?.message}
-                  isDisabled={field.disabled}
-                  autoComplete="new-password"
-                />
-              )}
-            />
-          </div>
-        </div>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="grid grid-cols-1 gap-2">
-            <p>Application name</p>
-            <Controller
-              name="app"
-              control={control}
-              rules={{ required: 'Apllication name is required' }}
-              disabled={isConnecting}
-              render={({ field, fieldState: { error } }) => (
-                <Input
-                  placeholder="Enter application name"
-                  variant="bordered"
-                  size="lg"
-                  value={field.value}
-                  onChange={field.onChange}
-                  classNames={{ input: 'text-base px-2' }}
-                  errorMessage={error?.message}
-                  isInvalid={!!error?.message}
-                  isDisabled={field.disabled}
-                />
-              )}
-            />
+          <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 gap-2">
+              <p>Application name</p>
+              <Controller
+                name="app"
+                control={control}
+                rules={{ required: 'Apllication name is required' }}
+                disabled={isConnecting}
+                render={({ field, fieldState: { error } }) => (
+                  <Input
+                    placeholder="Enter application name"
+                    variant="bordered"
+                    size="lg"
+                    value={field.value}
+                    onChange={field.onChange}
+                    classNames={{ input: 'text-base px-2' }}
+                    errorMessage={error?.message}
+                    isInvalid={!!error?.message}
+                    isDisabled={field.disabled}
+                  />
+                )}
+              />
+            </div>
           </div>
         </div>
         <div className="mt-10 space-x-4 flex items-center">
@@ -202,13 +202,17 @@ const Form = ({ onPrev, onNext }) => {
   );
 };
 
-const Tables = ({ onPrev, onNext }) => {
+const Collections = ({ onPrev, onNext }) => {
   const [selected, setSelected] = useState([]);
   const tables = useCreateProjectStore((state) => state.data.tables);
 
   return (
     <div>
-      <Title title="Select tables" sub="Select the tables you want to include in your analysis" className="mb-10" />
+      <Title
+        title="Select collections"
+        sub="Select the collections you want to include in your analysis"
+        className="mb-10"
+      />
       <div className="flex flex-wrap gap-6">
         {tables.map((table, index) => (
           <div key={index} className="border border-default-200 rounded-2xl px-4 py-2">
@@ -253,7 +257,7 @@ ConnectMongodb.propTypes = {
   onPrev: PropTypes.func,
   onNext: PropTypes.func,
 };
-Tables.propTypes = {
+Collections.propTypes = {
   onPrev: PropTypes.func,
   onNext: PropTypes.func,
 };
@@ -263,4 +267,3 @@ Form.propTypes = {
 };
 
 export default ConnectMongodb;
-
