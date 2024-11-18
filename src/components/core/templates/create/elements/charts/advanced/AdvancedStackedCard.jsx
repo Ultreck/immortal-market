0ac30bba8bbb44666808348e35a84a239914de4e
Praise <oluwadhammieh@gmail.com>
2024-11-18 12:58:55 +0100

    Feat(Charts): AdvancedStackedCard.jsx alignment removed comments

--- src/components/core/templates/create/elements/charts/advanced/AdvancedStackedCard.jsx
+++ src/components/core/templates/create/elements/charts/advanced/AdvancedStackedCard.jsx
@@ -84,69 +84,6 @@ export const AdvancedStackedCardContent = ({ element }) => {
       ))}
     </div>
   );
-  // return (
-  //   <div
-  //     style={{
-  //       display: 'flex',
-  //       flexDirection: 'column',
-  //       alignItems: justifyContentMap[alignment],
-  //       paddingTop: element.config.styles.yPadding,
-  //       paddingLeft: element.config.styles.xPadding,
-  //       paddingBottom: element.config.styles.yPadding,
-  //       paddingRight: element.config.styles.xPadding,
-  //       width: element.width,
-  //       height: element.height,
-  //       opacity: element.style.opacity,
-  //     }}
-  //   >
-  //     {data.slice(0, bars).map((item, index) => (
-  //       <motion.div
-  //         key={item.range}
-  //         className={`rounded-lg p-4 text-center`}
-  //         initial={{ opacity: 0, scale: 0.8 }}
-  //         animate={{ opacity: 1, scale: 1 }}
-  //         transition={{ duration: 0.2 }}
-  //         style={{
-  //           width: `${85 + index * 5}%`,
-  //           marginLeft: `${7.5 - index * 2.5}%`,
-  //           backgroundColor: colors[index],
-  //           fontFamily: 'Arial, sans-serif',
-  //         }}
-  //         whileHover={{ scale: 1.1 }}
-  //       >
-  //         <>
-  //           <div
-  //             style={{
-  //               fontFamily: 'Arial, sans-serif',
-  //               fontWeight: element.config.styles.lFontWeight,
-  //               fontStyle: element.config.styles.lFontStyle,
-  //               fontSize: element.config.styles.labelSize || '18px',
-  //               color: element.config.styles.valueAndLableColor,
-  //             }}
-  //             className="text-sm mb-1 text-black"
-  //           >
-  //             {item.range}
-  //           </div>
-  //           <motion.div
-  //             className="font-bold text-black"
-  //             initial={{ scale: 0 }}
-  //             animate={{ scale: 1 }}
-  //             transition={{ duration: 0.5, delay: index * 0.1 + 0.2 }}
-  //             style={{
-  //               fontFamily: 'Verdana, sans-serif',
-  //               fontWeight: element.config.styles.lFontWeight,
-  //               fontStyle: element.config.styles.lFontStyle,
-  //               fontSize: element.config.styles.valueSize || '18px',
-  //               color: element.config.styles.valueAndLableColor,
-  //             }}
-  //           >
-  //             {item.percentage}%
-  //           </motion.div>
-  //         </>
-  //       </motion.div>
-  //     ))}
-  //   </div>
-  // );
 };
 
 AdvancedStackedCardContent.propTypes = {
