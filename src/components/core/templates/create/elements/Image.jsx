eb8a2c7c93322e9937a96a27aeffb07c8bb8525b
Jude <judecodes@gmail.com>
2024-11-22 05:56:55 +0100

    fix(design): tailwind classes warning

--- src/components/core/templates/create/elements/Image.jsx
+++ src/components/core/templates/create/elements/Image.jsx
@@ -1,4 +1,3 @@
-import { Image as NextImage } from '@nextui-org/react';
 import { ElementPropTypes } from '@/lib/prop-types.js';
 import PropTypes from 'prop-types';
 import { cn } from '@/lib/utils.js';
@@ -13,31 +12,31 @@ export const ImagePresent = ({ element }) => {
 
 const classes = {
   'img-hover-zoom--quick-zoom': {
-    img: 'origin-[0_0] transition-transform duration-[250ms] group-hover:scale-[2]',
+    img: 'origin-top-left transition-[transform_0.25s,visibility_0.25s] duration-300 group-hover:[transform:scale(2)]',
   },
   'img-hover-zoom--point-zoom': {
-    img: 'origin-[65%_75%] transition-transform duration-[1000ms] ease-out group-hover:scale-[5]',
+    img: '[transform-origin:65%_75%] transition-[transform,filter] duration-1000 ease-out group-hover:[transform:scale(5)]',
   },
   'img-hover-zoom--zoom-n-rotate': {
-    img: 'transition-transform duration-[500ms] ease-in-out group-hover:scale-[2] group-hover:rotate-[25deg]',
+    img: 'transition-transform duration-500 ease-in-out group-hover:[transform:scale(2)_rotate(25deg)]',
   },
   'img-hover-zoom--slowmo': {
-    img: 'origin-[50%_65%] transition-transform duration-[5000ms] filter brightness-[1.5] transition-filter duration-[3000ms] ease-in-out group-hover:scale-[3] group-hover:brightness-[1]',
+    img: '[transform-origin:50%_65%] transition-[transform,filter] duration-1000 ease-in-out [filter:brightness(150%)] group-hover:[transform:scale(3)] group-hover:[filter:brightness(100%)]',
   },
   'img-hover-zoom--brightness': {
-    img: 'origin-center transition-transform duration-[2000ms] filter brightness-[0.5] transition-filter duration-[1500ms] ease-in-out group-hover:scale-[1.3] group-hover:brightness-[1]',
+    img: 'transition-[transform,filter] duration-1000 ease-in-out [transform-origin:center] [filter:brightness(50%)] group-hover:[filter:brightness(100%)] group-hover:[transform:scale(1.3)]',
   },
   'img-hover-zoom--zoom-n-pan-h': {
-    img: 'origin-[100%_0] scale-[1.4] transition-transform duration-[500ms] ease-in-out group-hover:scale-[1.5] group-hover:translate-x-[30%]',
+    img: 'transition-transform duration-500 ease-in-out [transform:scale(1.4)] origin-right group-hover:[transform:scale(1.5)_translateX(30%)]',
   },
   'img-hover-zoom--zoom-n-pan-v': {
-    img: 'origin-[0_0] scale-[1.4] transition-transform duration-[500ms] ease-in-out group-hover:scale-[1.25] group-hover:translate-y-[-30%]',
+    img: 'transition-transform duration-500 ease-in-out [transform:scale(1.4)] origin-top group-hover:[transform:scale(1.25)_translateY(-30%)]',
   },
   'img-hover-zoom--blur': {
-    img: 'scale-[1.2] transition-transform duration-[1000ms] filter blur-[2px] transition-filter duration-[2000ms] ease-in-out group-hover:scale-[1] group-hover:blur-[0]',
+    img: 'transition-[transform,filter] duration-1000 ease-in-out [filter:blur(2px)] [transform:scale(1.2)] group-hover:[filter:blur(0)] group-hover:[transform:scale(1)]',
   },
   'img-hover-zoom--colorize': {
-    img: 'filter grayscale transition-transform duration-[500ms] transition-filter duration-[1500ms] ease-in-out group-hover:scale-[1.1] group-hover:grayscale-0',
+    img: 'transition-[transform,filter] duration-500 ease-in-out [filter:grayscale(100%)] group-hover:[filter:grayscale(0)] group-hover:[transform:scale(1.1)]',
   },
 };
 
@@ -47,15 +46,14 @@ const ImageContent = ({ element }) => {
       className={cn('h-full w-full relative overflow-hidden group')}
       style={{ filter: `drop-shadow(${element.style.shadow})` }}
     >
-      <NextImage
+      <img
         src={element.config.src}
         alt={element.text}
         className={cn(
           'w-full h-full object-cover rounded-none z-[0]',
           element.config?.hover ? classes[element.config.hover]?.img || '' : ''
         )}
-        removeWrapper
-        style={{ ...element.style, transform: undefined }}
+        style={{ transform: undefined }}
       />
     </div>
   );
