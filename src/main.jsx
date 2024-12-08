34dae2c134011a124c446c210180462871351cc7
Jude <judecodes@gmail.com>
2024-12-08 09:44:51 +0100

    chore: disable react strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -7,12 +6,10 @@ import Providers from '@/Providers.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Providers>
-        <App />
-        <ResizeScreenHeight />
-      </Providers>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Providers>
+      <App />
+      <ResizeScreenHeight />
+    </Providers>
+  </BrowserRouter>
 );
