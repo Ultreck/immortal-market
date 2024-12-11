4331d0b6908f6e170a6e7805bc4f56e1e396b68d
Jude <judecodes@gmail.com>
2024-12-11 13:33:33 +0100

    chore: remove unused ProjectTable and messaging components

--- src/components/core/GlobalModals.jsx
+++ src/components/core/GlobalModals.jsx
@@ -1,11 +1,9 @@
 import CreateProjectModal from '@/components/core/project/CreateProjectModal.jsx';
-import MessagingModal from '../../pages/business/Messaging';
 
 const GlobalModals = () => {
   return (
     <>
       <CreateProjectModal />
-      <MessagingModal />
     </>
   );
 };
