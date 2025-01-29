098ccaa66f408a3d012c39299efff680fbc5eec8
Jude <judecodes@gmail.com>
2025-01-29 01:25:22 +0100

    feat: implement EditDesignTitleButton for updating design title & add tags

--- src/components/core/templates/create/footer/DesignTitle.jsx
+++ src/components/core/templates/create/footer/DesignTitle.jsx
@@ -1,74 +1,21 @@
 import useTemplateStore from '@/store/template.js';
-import { cn } from '@/lib/utils.js';
-import { useCallback, useEffect, useState } from 'react';
-import { useGetDesign, useUpdateDesign } from '@/api/business.js';
+import { useGetDesign } from '@/api/business.js';
 import useBusiness from '@/hooks/use-business.js';
-import { useToast } from '@/hooks/use-toast.jsx';
-import { useQueryClient } from '@tanstack/react-query';
-import { Chip, Spinner } from '@heroui/react';
-import { AnimatePresence, motion } from 'motion/react';
+import { Chip } from '@heroui/react';
+import EditDesignTitleButton from '@/components/core/templates/create/footer/EditDesignTitleButton.jsx';
 
 const DesignTitle = () => {
-  const toast = useToast();
-  const qc = useQueryClient();
   const { id: business } = useBusiness();
   const id = useTemplateStore((state) => state.template.id);
   const { data: { design = {} } = {} } = useGetDesign(business, id);
-  const { mutateAsync: update, isPending: isUpdateLoading } = useUpdateDesign(business, id);
-  const [value, setValue] = useState('');
-  const [isEditing, setIsEditing] = useState(false);
-
-  useEffect(() => {
-    setValue(design.title);
-  }, [design]);
-
-  const handleSave = useCallback(async () => {
-    if (value === design.title) return setIsEditing(false);
-    try {
-      await update({ title: value });
-      toast.success('Title updated');
-      await qc.invalidateQueries({ queryKey: ['businesses', business, 'designs'] });
-    } catch (error) {
-      toast.error(error?.response?.data?.message || error.message);
-    }
-    setIsEditing(false);
-  }, [value, design.title, update, toast, qc, business]);
 
   return (
     <div className="flex items-center space-x-4">
       <div className="relative flex items-center space-x-2">
-        <AnimatePresence mode="wait">
-          {isEditing ? (
-            <motion.input
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              required
-              className={cn(
-                'border border-default-200 bg-transparent text-lg leading-tight w-[200px] py-2 px-3 rounded-2xl'
-              )}
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              onBlur={() => handleSave()}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') e.currentTarget.blur();
-              }}
-              disabled={isUpdateLoading}
-              autoFocus
-            />
-          ) : (
-            <motion.p
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              onClick={() => setIsEditing(true)}
-              className="text-lg font-medium leading-tight border border-transparent hover:border-default-200 py-2 px-3 rounded-2xl"
-            >
-              {design.title}
-            </motion.p>
-          )}
-        </AnimatePresence>
-        {isUpdateLoading && <Spinner size="sm" />}
+        <div className="flex items-center space-x-2">
+          <p className="text-lg font-medium leading-tight">{design.title}</p>
+          <EditDesignTitleButton />
+        </div>
       </div>
       {design.type === 'template' && (
         <div className="flex items-center space-x-2">
---
+++ src/components/core/templates/create/footer/EditDesignTitleButton.jsx
@@ -0,0 +1,110 @@
+import { Button, Input, Popover, PopoverContent, PopoverTrigger, useDisclosure } from '@heroui/react';
+import { useToast } from '@/hooks/use-toast.jsx';
+import useBusiness from '@/hooks/use-business.js';
+import useTemplateStore from '@/store/template.js';
+import { useGetDesign, useUpdateDesign } from '@/api/business.js';
+import { Controller, useForm } from 'react-hook-form';
+import { RiPencilLine } from 'react-icons/ri';
+
+const EditDesignTitleButton = () => {
+  const toast = useToast();
+  const { isOpen, onOpenChange } = useDisclosure();
+  const { id: business } = useBusiness();
+  const id = useTemplateStore((state) => state.template.id);
+  const { data: { design = {} } = {} } = useGetDesign(business, id);
+  const { mutateAsync: update, isPending: isUpdateLoading } = useUpdateDesign(business, id);
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      title: design.title,
+      tags: design.tags?.join(',') || '',
+    },
+  });
+
+  const submit = async (values) => {
+    try {
+      if (values.tags) values.tags = values.tags.split(',');
+      await update(values);
+      toast.success('Title updated');
+      onOpenChange();
+    } catch (e) {
+      toast.error(e?.response?.data?.message ?? e?.message ?? 'Something went wrong, please try again');
+    }
+  };
+
+  return (
+    <>
+      <Popover
+        placement="top"
+        showArrow
+        offset={10}
+        classNames={{ content: 'w-[350px] block' }}
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      >
+        <PopoverTrigger>
+          <Button isIconOnly radius="full" variant="light" size="sm">
+            <RiPencilLine size="18" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0 shadow border border-default-200">
+          <div className="px-8 py-6">
+            <h3 className="text-lg font-semibold mb-6">Update design</h3>
+            <form onSubmit={handleSubmit(submit)}>
+              <div className="space-y-4">
+                <Controller
+                  name="title"
+                  control={control}
+                  rules={{ required: 'Title is required' }}
+                  disabled={isUpdateLoading}
+                  render={({ field, fieldState: { error } }) => (
+                    <div>
+                      <p className="mb-1 px-1">Title</p>
+                      <Input
+                        placeholder="Title"
+                        className="text-base"
+                        size="lg"
+                        classNames={{ input: 'text-base px-2' }}
+                        {...field}
+                        isInvalid={!!error?.message}
+                        error={error?.message}
+                        isDisabled={field.disabled}
+                      />
+                    </div>
+                  )}
+                />
+                <Controller
+                  name="tags"
+                  control={control}
+                  rules={{ required: 'Tags is required' }}
+                  disabled={isUpdateLoading}
+                  render={({ field, fieldState: { error } }) => (
+                    <div>
+                      <p className="mb-1 px-1">Tags</p>
+                      <Input
+                        placeholder="Tags"
+                        className="text-base"
+                        size="lg"
+                        classNames={{ input: 'text-base px-2' }}
+                        {...field}
+                        isInvalid={!!error?.message}
+                        error={error?.message}
+                        isDisabled={field.disabled}
+                      />
+                    </div>
+                  )}
+                />
+              </div>
+              <div className="mt-6">
+                <Button color="success" type="submit" isLoading={isUpdateLoading} className="text-base" radius="full">
+                  Save
+                </Button>
+              </div>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+};
+
+export default EditDesignTitleButton;
