2ba5c32da0f549bcbff81b3879aef7c701f6bf9d
Jude <judecodes@gmail.com>
2025-01-30 21:11:38 +0100

    feat(design): adjust padding in elements list for improved layout

--- src/components/core/templates/create/sidebar/components/Elements.jsx
+++ src/components/core/templates/create/sidebar/components/Elements.jsx
@@ -47,7 +47,7 @@ const Elements = () => {
         <div className="space-y-8">
           {items.map((item, index) => (
             <div key={index}>
-              <div className="flex items-center justify-between mb-4 bg-white/5 rounded-2xl px-4 py-1.5">
+              <div className="flex items-center justify-between mb-4 bg-white/5 rounded-full px-5 py-2">
                 <h4 className="text-base font-semibold">{item.name}</h4>
                 {item.view && (
                   <Button
